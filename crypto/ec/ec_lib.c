727e191986578951d4b1fdf453cdf27897f6f66f
Haruki Gonai <hg2541@columbia.edu>
2023-04-06 14:54:46 +0000

    Prints in EC_GROUP_COPY

--- crypto/ec/ec_lib.c
+++ crypto/ec/ec_lib.c
@@ -170,78 +170,112 @@ void EC_GROUP_clear_free(EC_GROUP *group)
 
 int EC_GROUP_copy(EC_GROUP *dest, const EC_GROUP *src)
 	{
+printf("%s: %d\n", __func__, __LINE__);
 	EC_EXTRA_DATA *d;
-
+printf("%s: %d\n", __func__, __LINE__);
 	if (dest->meth->group_copy == 0)
 		{
+printf("%s: %d\n", __func__, __LINE__);
 		ECerr(EC_F_EC_GROUP_COPY, ERR_R_SHOULD_NOT_HAVE_BEEN_CALLED);
+printf("%s: %d\n", __func__, __LINE__);
 		return 0;
 		}
+printf("%s: %d\n", __func__, __LINE__);
 	if (dest->meth != src->meth)
 		{
+printf("%s: %d\n", __func__, __LINE__);
 		ECerr(EC_F_EC_GROUP_COPY, EC_R_INCOMPATIBLE_OBJECTS);
+printf("%s: %d\n", __func__, __LINE__);
 		return 0;
 		}
 	if (dest == src)
 		return 1;
-	
+printf("%s: %d\n", __func__, __LINE__);
 	EC_EX_DATA_free_all_data(&dest->extra_data);
-
+printf("%s: %d\n", __func__, __LINE__);
 	for (d = src->extra_data; d != NULL; d = d->next)
 		{
+printf("%s: %d\n", __func__, __LINE__);
 		void *t = d->dup_func(d->data);
-		
+printf("%s: %d\n", __func__, __LINE__);
 		if (t == NULL)
 			return 0;
+printf("%s: %d\n", __func__, __LINE__);
 		if (!EC_EX_DATA_set_data(&dest->extra_data, t, d->dup_func, d->free_func, d->clear_free_func))
 			return 0;
+printf("%s: %d\n", __func__, __LINE__);
 		}
-
+printf("%s: %d\n", __func__, __LINE__);
 	if (src->generator != NULL)
 		{
+printf("%s: %d\n", __func__, __LINE__);
 		if (dest->generator == NULL)
 			{
+printf("%s: %d\n", __func__, __LINE__);
 			dest->generator = EC_POINT_new(dest);
+printf("%s: %d\n", __func__, __LINE__);
 			if (dest->generator == NULL) return 0;
+printf("%s: %d\n", __func__, __LINE__);
 			}
+printf("%s: %d\n", __func__, __LINE__);
 		if (!EC_POINT_copy(dest->generator, src->generator)) return 0;
+printf("%s: %d\n", __func__, __LINE__);
 		}
 	else
 		{
+printf("%s: %d\n", __func__, __LINE__);
 		/* src->generator == NULL */
 		if (dest->generator != NULL)
 			{
+printf("%s: %d\n", __func__, __LINE__);
 			EC_POINT_clear_free(dest->generator);
+printf("%s: %d\n", __func__, __LINE__);
 			dest->generator = NULL;
 			}
 		}
-
+printf("%s: %d\n", __func__, __LINE__);
 	if (!BN_copy(&dest->order, &src->order)) return 0;
+printf("%s: %d\n", __func__, __LINE__);
 	if (!BN_copy(&dest->cofactor, &src->cofactor)) return 0;
+printf("%s: %d\n", __func__, __LINE__);
 
 	dest->curve_name = src->curve_name;
 	dest->asn1_flag  = src->asn1_flag;
 	dest->asn1_form  = src->asn1_form;
-
+printf("%s: %d\n", __func__, __LINE__);
 	if (src->seed)
 		{
-		if (dest->seed)
+printf("%s: %d\n", __func__, __LINE__);
+		if (dest->seed) {
 			OPENSSL_free(dest->seed);
+printf("%s: %d\n", __func__, __LINE__);
+		}
+printf("%s: %d\n", __func__, __LINE__);
 		dest->seed = OPENSSL_malloc(src->seed_len);
-		if (dest->seed == NULL)
+printf("%s: %d\n", __func__, __LINE__);
+		if (dest->seed == NULL) {
+printf("%s: %d\n", __func__, __LINE__);
 			return 0;
-		if (!memcpy(dest->seed, src->seed, src->seed_len))
+		}
+printf("%s: %d\n", __func__, __LINE__);
+		if (!memcpy(dest->seed, src->seed, src->seed_len)) {
+printf("%s: %d\n", __func__, __LINE__);
 			return 0;
+		}
+printf("%s: %d\n", __func__, __LINE__);
 		dest->seed_len = src->seed_len;
 		}
 	else
 		{
-		if (dest->seed)
+printf("%s: %d\n", __func__, __LINE__);
+		if (dest->seed) {
+printf("%s: %d\n", __func__, __LINE__);
 			OPENSSL_free(dest->seed);
+		}
 		dest->seed = NULL;
 		dest->seed_len = 0;
 		}
-	
+printf("%s: %d\n", __func__, __LINE__);
 
 	return dest->meth->group_copy(dest, src);
 	}
